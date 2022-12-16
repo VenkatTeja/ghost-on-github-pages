a5f67fe8f0277387cbaf2f13f3d164ebe4dabe27
VenkatTeja <teja.kunisetty@gmail.com>
2022-12-16 17:36:03 +0530

    change admin db path

--- email/send-invite.js
+++ email/send-invite.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('../../.ghost3/admin.db');
+const db = new sqlite3.Database('../../.ghost/admin.db');
 const fs = require('fs');
 const path = require('path');
 const sendMail = require('./gmail');
